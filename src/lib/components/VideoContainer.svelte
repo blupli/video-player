cf6d3cf2d16d2fc68877bc3205c5ffb534d5e1a2
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2024-11-23 13:19:13 +0300

    Implementing base video container component.

---
+++ src/lib/components/VideoContainer.svelte
@@ -0,0 +1,102 @@
+<script lang="ts">
+	import { onMount, type Snippet } from 'svelte';
+
+	type Props = {
+		videos: {
+			src: string;
+			title: string;
+			description: string;
+		}[];
+		selectedVideoIndex?: number;
+		volume?: number;
+
+		onVideoReady?: (videoElement: HTMLVideoElement) => void;
+		children?: Snippet<[]>;
+
+		width: string | number;
+		height: string | number;
+		background?: string;
+	};
+
+	let {
+		videos,
+		selectedVideoIndex = $bindable(0),
+		volume = $bindable(0.5),
+		onVideoReady,
+		children,
+		width,
+		height,
+		background = 'black'
+	}: Props = $props();
+
+	let selectedVideo = $derived(videos[selectedVideoIndex]);
+
+	let videoElement: HTMLVideoElement | null = $state(null);
+
+	function modifyVideoIndex(offset: number) {
+		selectedVideoIndex = (selectedVideoIndex + offset) % videos.length;
+	}
+
+	function onVideoEnded() {
+		modifyVideoIndex(1);
+	}
+
+	onMount(() => {
+		if (videoElement && onVideoReady) onVideoReady(videoElement);
+	});
+</script>
+
+<div
+	class="video-container"
+	style:--width={typeof width === 'number' ? `${width}px` : width}
+	style:--height={typeof height === 'number' ? `${height}px` : height}
+	style:background
+>
+	{#if selectedVideo}
+		<video
+			class="source-video"
+			bind:this={videoElement}
+			src={selectedVideo.src}
+			{volume}
+			onended={onVideoEnded}
+			autoplay
+		>
+			<track kind="captions" src="captions.vtt" srclang="en" label="English" />
+		</video>
+	{/if}
+	<div class="gradient-overlay"></div>
+</div>
+
+<style lang="scss">
+	.video-container {
+		position: relative;
+		width: var(--width);
+		height: var(--height);
+		background-color: black;
+
+		& > .source-video,
+		& > .gradient-overlay {
+			position: absolute;
+			width: var(--width);
+			height: var(--height);
+			inset: 0;
+		}
+
+		& > .source-video {
+			z-index: 1;
+			object-fit: contain;
+		}
+
+		& > .gradient-overlay {
+			z-index: 2;
+			background: linear-gradient(
+				180deg,
+				rgba(0, 0, 0, 0.75),
+				rgba(0, 0, 0, 0.25),
+				rgba(0, 0, 0, 0),
+				rgba(0, 0, 0, 0.25),
+				rgba(0, 0, 0, 0.75)
+			);
+		}
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,38 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import VideoContainer from '$lib/components/VideoContainer.svelte';
+
+	const videos = [
+		{
+			title: 'Interactive Installations',
+			src: 'https://blupli.com/content/videos/home/1.mp4',
+			description: 'Live Interactive Style transfer'
+		},
+		{
+			title: 'Raves',
+			src: 'https://blupli.com/content/videos/home/2.mp4',
+			description: 'DJ Performance-Driven Visuals Based on Music Analysis'
+		},
+		{
+			title: 'Concerts',
+			src: 'https://blupli.com/content/videos/home/3.mp4',
+			description: 'Live Lyric detection to Generative Visuals'
+		},
+		{
+			title: 'Interactive Installations',
+			src: 'https://blupli.com/content/videos/home/4.mp4',
+			description: 'Live Interactive Style transfer'
+		},
+		{
+			title: 'Concerts',
+			src: 'https://blupli.com/content/videos/home/5.mp4',
+			description: 'Live Lyric detection to Visuals'
+		},
+		{
+			title: 'Conferences',
+			src: 'https://blupli.com/content/videos/home/6.mp4',
+			description: 'Speech Detection to Storytelling Visuals'
+		}
+	];
+</script>
+
+<VideoContainer width="100%" height="75vh" {videos} background="purple" volume={0} />
